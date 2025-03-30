d4fda2dd8cf0a0182d148b95302a3a2693e28ebe
luong-04 <137810583+luong-04@users.noreply.github.com>
2025-03-30 21:38:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -74,4 +74,5 @@ def video_info():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 10000))  # Render cấp cổng tự động
+    app.run(host='0.0.0.0', port=port)
